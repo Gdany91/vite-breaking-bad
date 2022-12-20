fe81d67bf16a447738ed8b1d67823c7e941efc91
gdany91 <you@example.comdgarofalo52@gmail.com>
2022-12-20 18:01:13 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+74 lines (63 sloc) 1.24 KB
+<script >
+import axios from "axios"
+import { store } from "./store.js"
+import AppHeader from './components/AppHeader.vue'
+
+export default {
+  name: 'App',
+  components: {
+    AppHeader,
+   
+  },
+  data() {
+    return {
+      store,
+      
+    }
+  },
+}
+</script>
+
+<template>
+  
+  <header>
+    <AppHeader />
+  </header>
+
+  <main>
+
+  </main>
+</template>
+
+<style lang="scss">
+
+@use "./styles/general.scss" as *;
+
+
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,45 @@
+<script >
+export default {
+    name: 'AppHeader',
+    
+    data() {
+        return {
+        }
+    }
+}
+
+</script>
+
+<template>
+  <div class="container-header">
+    <img src="../..//images.jpg" alt="rick n morty" >
+    <h1>Rick n Morty API</h1>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+
+.container-header {
+  background-color: #191919;
+  width: 25%;
+  height: 70px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 80px;
+  margin-top: 70px;
+  margin: auto;
+  border-radius: 50px;
+
+  h1 {
+    color: rgb(255, 0, 98);
+    font-size: 30px;
+    font-weight: bold;
+  }
+  img {
+    width: 120px;
+    height: 70px;
+  }
+}
+</style>
